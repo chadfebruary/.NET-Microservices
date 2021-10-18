ed04a63f3f2512ed103a897e38ca0280c407085f
Chad February <chad@codeforte.com>
2021-10-18 09:57:08 +0200

    Initial commit.

---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,14 @@
+using PlatformService.Models;
+using System.Collections.Generic;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int Id);
+        void CreatePlatform(Platform plat);
+    }
+}
